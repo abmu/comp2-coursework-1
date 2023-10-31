adce87dd9aea7c189aa5b54c1f73ec62d27cb14f
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2023-10-31 18:34:20 +0000

    Draw grid

--- robot_search.c
+++ robot_search.c
@@ -1,6 +1,40 @@
 #include "./graphics.h"
+#include <stdio.h>
+
+const int winSize = 800;
+const int tileSize = 60;
+const int gridOffset = 100;
+const int gridHeight = 10;
+const int gridWidth = 10;
+const int grid[][10] = {
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0}
+};
+
+void drawGrid(void) {
+    foreground();
+    setColour(black);
+    for (int i = 0; i < gridHeight; i++) {
+        int y = (i * tileSize) + gridOffset;
+        for (int j = 0; j < gridWidth; j++) {
+            int x = (j * tileSize) + gridOffset;
+            if (grid[i][j] == 0) {
+                drawRect(x,y,tileSize,tileSize);
+            }
+        }
+    }
+}
 
 int main(void) {
-
+    setWindowSize(winSize,winSize);
+    drawGrid();
     return 0;
 }
